9ed9379a922fc96c132dbc3ffaffce95bbfc03ab
Lucas Madruga <Lucas_pump.it.up@hotmail.com>
2024-01-21 17:29:15 -0300

    Criado o arquivo para analise

---
+++ PrimeiraClasse.java
@@ -0,0 +1,16 @@
+package Br.com.lucas;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        String ambiente = args[0];
+		
+		if (ambiente.equals("DEV")) 
+        {
+        	System.out.print("Hello, Lucas Madruga");
+	    }
+
+    }
+}
